e56580f05f5b0a165b20dc351299de5fc9fda952
Maxim <99975552+maxim-epam@users.noreply.github.com>
2025-06-29 20:25:06 +0200

    refactor: change Set-Variable to New-Variable for script scope constants

--- DevSetup/Modules/Constants.psm1
+++ DevSetup/Modules/Constants.psm1
@@ -6,22 +6,22 @@
 #>
 
 # Script metadata
-Set-Variable -Name SCRIPT_VERSION -Value "1.0.0" -Option ReadOnly -Scope Global
-Set-Variable -Name LOG_DATE_FORMAT -Value "yyyy-MM-dd HH:mm:ss" -Option ReadOnly -Scope Global
-Set-Variable -Name SUMMARY_SEPARATOR -Value ("=" * 60) -Option ReadOnly -Scope Global
+New-Variable -Name SCRIPT_VERSION -Value "1.0.0" -Option ReadOnly -Scope Script -Force
+New-Variable -Name LOG_DATE_FORMAT -Value "yyyy-MM-dd HH:mm:ss" -Option ReadOnly -Scope Script -Force
+New-Variable -Name SUMMARY_SEPARATOR -Value ("=" * 60) -Option ReadOnly -Scope Script -Force
 
 # Module and file names
-Set-Variable -Name DSC_MODULE_NAME -Value "PSDscResources" -Option ReadOnly -Scope Global
-Set-Variable -Name CONFIG_FILE_NAME -Value "configuration.dsc.yaml" -Option ReadOnly -Scope Global
-Set-Variable -Name DSC_FOLDER_NAME -Value "DscCustom" -Option ReadOnly -Scope Global
+New-Variable -Name DSC_MODULE_NAME -Value "PSDscResources" -Option ReadOnly -Scope Script -Force
+New-Variable -Name CONFIG_FILE_NAME -Value "configuration.dsc.yaml" -Option ReadOnly -Scope Script -Force
+New-Variable -Name DSC_FOLDER_NAME -Value "DscCustom" -Option ReadOnly -Scope Script -Force
 
 # Log levels
-Set-Variable -Name LOG_LEVELS -Value @{
+New-Variable -Name LOG_LEVELS -Value @{
     "ERROR" = 0
     "WARN"  = 1
     "INFO"  = 2
     "DEBUG" = 3
-} -Option ReadOnly -Scope Global
+} -Option ReadOnly -Scope Script -Force
 
 # Export constants for use in other modules
 Export-ModuleMember -Variable SCRIPT_VERSION, LOG_DATE_FORMAT, SUMMARY_SEPARATOR, DSC_MODULE_NAME, CONFIG_FILE_NAME, DSC_FOLDER_NAME, LOG_LEVELS
